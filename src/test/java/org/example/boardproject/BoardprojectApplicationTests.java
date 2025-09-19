3e089a7c814967ec4e41cb7ba80edc0a88b84807
seongmin2223 <khyuno9@naver.com>
2025-09-19 10:07:47 +0900

    Initial commit

---
+++ src/test/java/org/example/boardproject/BoardprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
